546a2ed3e58034b6ef4ecdbaa279557ed4bb268f
木南 <2258650940@qq.com>
2026-09-13 21:22:25 +0800

    Update properties.sh

--- scripts/properties.sh
+++ scripts/properties.sh
@@ -464,7 +464,7 @@ TERMUX__REPOS_HOST_ORG_URL="https://github.com/$TERMUX__REPOS_HOST_ORG_NAME"
 #
 # Default value: `com.termux`
 ##
-TERMUX_APP__PACKAGE_NAME="com.termux"
+TERMUX_APP__PACKAGE_NAME="com.retmux"
 TERMUX_APP_PACKAGE="$TERMUX_APP__PACKAGE_NAME" # Deprecated alternative variable for `TERMUX_APP__PACKAGE_NAME`
 
 __termux_build_props__add_variables_validator_actions "TERMUX_APP__PACKAGE_NAME" "app_package_name"
@@ -1848,7 +1848,7 @@ TERMUX_APP__REPO_URL="$TERMUX__REPOS_HOST_ORG_URL/$TERMUX_APP__REPO_NAME"
 #
 # Default value: `com.termux`
 ##
-TERMUX_APP__NAMESPACE="com.termux"
+TERMUX_APP__NAMESPACE="com.retmux"
 
 __termux_build_props__add_variables_validator_actions "TERMUX_APP__NAMESPACE" "app_package_name"
 
@@ -1871,7 +1871,7 @@ __termux_build_props__add_variables_validator_actions "TERMUX_APP__APP_DIR" "saf
 # **See Also:**
 # - https://github.com/termux/termux-app/blob/master/app/src/main/java/com/termux/app/TermuxActivity.java
 #
-# Default value: `com.termux.app.TermuxActivity`
+# Default value: `.app.TermuxActivity`
 ##
 TERMUX_APP__SHELL_API__SHELL_API_ACTIVITY__CLASS_NAME="$TERMUX_APP__NAMESPACE.app.TermuxActivity"
 
@@ -2180,7 +2180,7 @@ TERMUX_AM_APP__AM_CLASS__CLASS_NAME="$TERMUX_AM_APP__NAMESPACE.Am"
 # and are compiled locally.
 # FIXME: Checking for all variables will be added later in repo
 # changes pull, currently only `TERMUX_REPO_APP__PACKAGE_NAME` is checked.
-TERMUX_REPO_APP__PACKAGE_NAME="com.termux"
+TERMUX_REPO_APP__PACKAGE_NAME="com.retmux"
 TERMUX_REPO_APP__DATA_DIR="/data/data/com.termux"
 TERMUX_REPO__CORE_DIR="/data/data/com.termux/termux/core"
 TERMUX_REPO__APPS_DIR="/data/data/com.termux/termux/app"
